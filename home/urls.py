f10b0261b679af7954fc9b20cc5df7ffca44ae4c
Nurpeiis Baimukan <nb2577@nyu.edu>
2018-10-13 19:28:39 +0400

    created form.html in the home app, whenever user inputs data it can be seen in the same page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include,  path
-from home.views import HomeView
+from home.views import HomeView, FormView
 
 app_name = 'home'
 urlpatterns = [
     path('', HomeView.as_view(), name = 'home' ), #login
+    path('form/', FormView.as_view(), name = 'form')
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from home.forms import HomeForm
-
+#from home.models import Post 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+
+class FormView(TemplateView):
+    template_name = 'home/form.html'
 
     def get(self, request):
         form = HomeForm()
@@ -15,5 +19,7 @@ class HomeView(TemplateView):
         if form.is_valid():
             #clean something like sql injections by code cleaned_data
             text = form.cleaned_data['post']
+            form = HomeForm()
+            return redirect ('home:form')
         args = {'form': form, 'text': text}   
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, args)
