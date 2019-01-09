88105facfde653f9385db9f90e1c3fd3e95c5aef
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-01-09 21:11:49 +0400

    Fixed navbar issue by changing style.css= deleting ul option, added google map in footer, changed sizes in RED_form.html

--- accounts/static/js/custom.js
+++ accounts/static/js/custom.js
@@ -3,37 +3,6 @@ jQuery(document).ready(function($) {
   "use strict";
 
 
-  (function() {
-
-    var $menu = $('.navigation nav'),
-      optionsList = '<option value="" selected>Go to..</option>';
-
-    $menu.find('li').each(function() {
-        var $this = $(this),
-          $anchor = $this.children('a'),
-          depth = $this.parents('ul').length - 1,
-          indent = '';
-
-        if (depth) {
-          while (depth > 0) {
-            indent += ' - ';
-            depth--;
-          }
-
-        }
-        $(".nav li").parent().addClass("bold");
-
-        optionsList += '<option value="' + $anchor.attr('href') + '">' + indent + ' ' + $anchor.text() + '</option>';
-      }).end()
-      .after('<select class="selectmenu">' + optionsList + '</select>');
-
-    $('select.selectmenu').on('change', function() {
-      window.location = $(this).val();
-    });
-
-  })();
-
-
   $('.toggle-link').each(function() {
     $(this).click(function() {
       var state = 'open'; //assume target is closed & needs opening
@@ -93,12 +62,6 @@ jQuery(document).ready(function($) {
 
 
 
-  //Navi hover
-  $('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-  }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-  });
 
   // tooltip
   $('.social-network li a, .options_box .color a').tooltip();
