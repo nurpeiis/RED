ce2f9413ead751825c441869618e3f7681a4b65e
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-01-27 18:15:00 +0400

    superuser change db

--- accounts/models.py
+++ accounts/models.py
@@ -8,10 +8,10 @@ class Companies(models.Model):
     def __str__(self):
         return self.company_name
 class User(AbstractUser):
-    organization = models.ForeignKey(Companies, on_delete = models.CASCADE, blank = False, null=False, default=1)
+    organization = models.ForeignKey(Companies, on_delete = models.CASCADE, blank = True, null=True, default=1)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
-    title = models.CharField (max_length=50, blank = False, null=False, default='')
+    title = models.CharField (max_length=50, blank = True, null=True, default='')
     #upload_to is specific to file/image upload, because they are large, uploading is optional 
     image = models.ImageField(upload_to = 'profile_image', blank = True, null=True)
     #return username in the userprofile page
