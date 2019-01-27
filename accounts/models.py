15f8ddf1509ac0ea0caca0c7f55c129d77c8c698
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-01-27 18:17:39 +0400

    further db change

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ class Companies(models.Model):
     def __str__(self):
         return self.company_name
 class User(AbstractUser):
-    organization = models.ForeignKey(Companies, on_delete = models.CASCADE, blank = True, null=True, default=1)
+    organization = models.ForeignKey(Companies, on_delete = models.CASCADE, blank = True, null=True, default='')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
     title = models.CharField (max_length=50, blank = True, null=True, default='')
