25a5e2a863e39bda979fedea7209d567aa052106
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-03-03 11:35:56 +0400

    Add arrange meeting and change STATIC_ROOT to the same folder

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include,  path
-from home.views import HomeNotAuthView, HomeView, StepOneView, AboutUsNotAuthView, AboutUsView
+from home.views import HomeNotAuthView, HomeView, StepOneView, AboutUsNotAuthView, AboutUsView, ArrangeMeeting
 
 app_name = 'home'
 urlpatterns = [
     path('not-auth/', HomeNotAuthView.as_view(), name = 'home_notauth'),
     path('', HomeView.as_view(), name = 'home' ), #login
     path('form/', StepOneView.as_view(), name = 'form'),
+    path('arrange-meeting/', ArrangeMeeting.as_view(), name = 'arrange_meeting'), 
     path('about-us-notauth/', AboutUsNotAuthView.as_view(), name='about_us_notauth'),
     path('about-us/', AboutUsView.as_view(), name='about_us'),
 ]
