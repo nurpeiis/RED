6d5e2fac1371ddbabe86f245ea28c250e6848aa5
Nurpeiis Baimukan <nb2577@nyu.edu>
2019-03-10 10:45:48 +0400

    Figured out static file issue

--- RED/settings/base.py
+++ RED/settings/base.py
@@ -4,7 +4,7 @@ from decouple import config, Csv
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  '../static/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = '../media/'
 MEDIA_URL = '/media/'
